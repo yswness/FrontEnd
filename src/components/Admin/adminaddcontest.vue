ca597d0bcff9b6b4f3fbce2de0a9600e752a112a
yswness <yswness@163.com>
2020-05-29 17:06:54 +0800

    修改了Admin页面，添加加入比赛题目功能

--- src/components/Admin/adminaddcontest.vue
+++ src/components/Admin/adminaddcontest.vue
@@ -1,13 +1,144 @@
 <template>
   <div id="admin-addcontest">
+
+    <el-dialog title="选择题目" :visible.sync="dialogIsAble">
+      <el-table :data="problemData" ref="problemTable" @current-change="handleCurrentChange" highlight-current-row>
+        <el-table-column property="problemID" label="题目ID" width="100px"></el-table-column>
+        <el-table-column property="title" label="题目名称"></el-table-column>
+      </el-table>
+    </el-dialog>
+
     <el-row>
-      <el-col :span="16" :offset="4">
-        加入比赛
+      <el-col :span="14" :offset="5">
+        <el-card class="admin-addcontest-card">
+          <el-form 
+            :model="dataForm"
+            :rules="rules"
+            label-width="100px"
+            ref="dataForm">
+            <el-form-item label="比赛编号"> 1 </el-form-item>
+            <el-form-item label="比赛名称" prop="title">
+              <el-input v-model="dataForm.title"></el-input>
+            </el-form-item>
+            <el-form-item label="创建人" prop="creator_id">
+              <el-input v-model="dataForm.creator_id"></el-input>
+            </el-form-item>
+            <el-form-item label="比赛时间" prop="dateRange">
+              <el-date-picker
+                v-model="dataForm.dateRange"
+                type="datetimerange"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                range-separator="至">
+              </el-date-picker>
+            </el-form-item>
+            <el-form-item label="比赛类型" prop="type">
+              <el-select v-model="dataForm.type" placeholder="选择比赛类型">
+                <el-option label="ACM" value="ACM"></el-option>
+                <el-option label="OI" value="OI"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="比赛权限" prop="auth">
+              <el-select v-model="dataForm.auth" placeholder="选择比赛权限">
+                <el-option label="Public" value="Public"></el-option>
+                <el-option label="Private" value="Private"></el-option>
+                <el-option label="Protect" value="Protect"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item class="admin-con-addpro" label="加入题目">
+              <el-button type="primary" @click="selectProblem()">选择一个题目</el-button>
+            </el-form-item>
+            <el-form-item
+              v-for="(problem, index) in dataForm.problems"
+              :label="'Problem ' + String.fromCharCode('A'.charCodeAt() + index)"
+              :key="problem.key">
+              <el-button>{{ 'P ' + '1000' }}</el-button>
+              <el-button class="admin-addcon-button" @click.prevent="deleteProblem(problem)">删除</el-button>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('dataForm')">确认</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
       </el-col>
     </el-row>
   </div>
 </template>
 <script>
+export default {
+  data() {
+    return {
+      dialogIsAble: false,
+      problemData: [{
+        problemID: 1001,
+        title: 'A + B'
+      }],
+      dataForm: {
+        contest_id: 0,
+        creator_id: '',
+        title: '',
+        dateRange: [],
+        problems: [],
+        type: '',
+        auth: ''
+      },
+      rules: {
+        title: [ { required: true, message: '请输入题目标题', trigger: 'blur' } ],
+        creator_id: [ { required: true, message: '请输入创建人', trigger: 'blur' } ],
+        type: [ { required: true, message: '请选择比赛类型', trigger: 'blur' } ],
+        auth: [ { required: true, message: '请选择比赛权限', trigger: 'blur' } ],
+        dateRange: [ { required: true, message: '请选择比赛时间', trigger: 'blur' } ]
+      },
+      currentRow: null
+    }
+  },
+  methods: {
+    selectProblem() {
+      this.dialogIsAble = true;
+    },
+    handleCurrentChange(val) {
+      if (!val) return;
+      this.dataForm.problems.push({
+        problemID: val.problemID,
+        key: Date.now()
+      })
+      this.$refs.problemTable.setCurrentRow(null);
+      this.dialogIsAble = false;
+    },
+    deleteProblem(problem) {
+      let index = this.dataForm.problems.indexOf(problem);
+      if (index !== -1) {
+        this.dataForm.problems.splice(index, 1);
+      }
+    },
+    submitForm(item) {
+      this.$refs[item].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    }
+  }
+  
+}
 </script>
 <style scope>
+.admin-addcontest-card {
+  margin-top: 20px;
+}
+.admin-addcon-col {
+  text-align: center;
+}
+.admin-addcon-button {
+  margin-left: 20px;
+}
+.admin-addcon-spl {
+  text-align: center;
+}
+.admin-con-addpro {
+  margin-top: 60px;
+}
 </style>
--- src/components/Admin/adminaddproblem.vue
+++ src/components/Admin/adminaddproblem.vue
@@ -1,13 +1,114 @@
 <template>
   <div id="admin-addproblem">
     <el-row>
-      <el-col :span="16" :offset="4">
-        加入题目
+      <el-col :span="14" :offset="5">
+        <el-card class="admin-addproblem-card">
+          <el-form 
+            :model="dataForm"
+            :rules="rules"
+            label-width="100px"
+            ref="dataForm">
+            <el-form-item label="题目编号"> 1001 </el-form-item>
+            <el-form-item label="题目名称" prop="title">
+              <el-input v-model="dataForm.title"></el-input>
+            </el-form-item>
+            <el-form-item label="题目描述" prop="description">
+              <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 8}" v-model="dataForm.description"></el-input>
+            </el-form-item>
+            <el-form-item label="输入描述" prop="input">
+              <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 8}" v-model="dataForm.input"></el-input>
+            </el-form-item>
+            <el-form-item label="输出描述" prop="output">
+              <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 8}" v-model="dataForm.output"></el-input>
+            </el-form-item>
+            <el-form-item label="Hint" prop="hint">
+              <el-input type="textarea" :autosize="{ minRows: 2, maxRows: 8}" v-model="dataForm.hint"></el-input>
+            </el-form-item>
+            <el-form-item
+              v-for="(sample, index) in dataForm.sample"
+              :label="'样例' + index"
+              :key="sample.key">
+              <el-col :span="2" class="admin-addpro-col"><span>输入</span></el-col>
+              <el-col :span="7">
+                <el-input type="textarea" v-model="sample.sinput"></el-input>
+              </el-col>
+              <el-col :span="2" class="admin-addpro-col"><span>输出</span></el-col>
+              <el-col :span="7">
+                <el-input type="textarea" v-model="sample.soutput"></el-input>
+              </el-col>
+              <el-button class="admin-addpro-button" @click.prevent="deleteSample(sample)">删除</el-button>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('dataForm')">确认</el-button>
+              <el-button @click="addSample">新增样例</el-button>
+              <el-button @click="resetForm('dataForm')">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
       </el-col>
     </el-row>
   </div>
 </template>
 <script>
+export default {
+  data() {
+    return {
+      dataForm: {
+        title: '',
+        description: '',
+        input: '',
+        output: '',
+        hint: '',
+        sample: [{
+          sinput: '',
+          soutput: ''
+        }]
+      },
+      rules: {
+        title: [ { required: true, message: '请输入题目标题', trigger: 'blur' } ],
+        description: [ { required: true, message: '请输入题目描述', trigger: 'blur' } ],
+        input: [ { required: true, message: '请写点输入描述, 如果不需要写个无输入也行', trigger: 'blur' } ],
+        output: [ { required: true, message: '请输入输出描述', trigger: 'blur' } ],
+        hint: [ { required: true, message: '请输入Hint', trigger: 'blur' } ]
+      }
+    }
+  },
+  methods: {
+    addSample() {
+      this.dataForm.sample.push({
+        sinput: '',
+        soutput: '',
+        key: Date.now()
+      });
+    },
+    deleteSample(item) {
+      let index = this.dataForm.sample.indexOf(item);
+      if (index !== -1) {
+        this.dataForm.sample.splice(index, 1);
+      }
+    },
+    submitForm(item) {
+      this.$refs[item].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(item) {
+      this.$refs[item].resetFields();
+    }
+  }
+  
+}
 </script>
 <style scope>
+.admin-addpro-col {
+  text-align: center;
+}
+.admin-addpro-button {
+  margin-left: 20px;
+}
 </style>
